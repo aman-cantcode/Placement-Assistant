24bb58927dcc561f1f91c5a55c7c5d6820ea6f84
Aman <aman.cant.code@gmail.com>
2026-06-14 22:56:34 +0530

    add multer middleware in routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.post(
+  "/register",
+  upload.none(), // Multer middleware to parse multipart/form-data (no files)
+  registerUser
+);
 
 export default router;
