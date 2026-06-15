99a719d4fbcc8884aeedf34753e7901e10d7afa2
Aman <aman.cant.code@gmail.com>
2026-06-15 11:57:34 +0530

    add auth middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    const authHeader = req.header("Authorization") || "";
+    const token = authHeader.startsWith("Bearer ") ? authHeader.replace("Bearer ", "") : null;
+
+    if(!token) {
+        throw new ApiError(401, "Unauthorized Request");
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    } catch {
+        throw new ApiError(401, "Access token expired or invalid");
+    }
+
+    const user = await User.findById(decoded._id).select(
+        "-password -refreshToken -resetPasswordToken -resetPasswordExpiry -resume.text"
+    );
+    if (!user) {
+        throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+});
