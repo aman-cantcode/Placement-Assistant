e267f2c83c9e43a49fc4548b50cbd1968efe7de6
Aman <aman.cant.code@gmail.com>
2026-06-16 00:31:13 +0530

    add user routes for handling user-related API requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,9 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
 export { app };
