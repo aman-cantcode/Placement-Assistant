bac0f2d214adc57c91a5f4cba1e0cc19c1cd19ce
Aman <aman.cant.code@gmail.com>
2026-06-16 21:54:56 +0530

    add history and chat models with schemas for message and chat structure

---
+++ backend/src/controllers/history.controller.js
@@ -0,0 +1,8 @@
+import {History} from '../models/history.model.js';
+import { ApiError } from '../utils/ApiError.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+import { AsyncHandler } from '../utils/AsyncHandler.js';
+import { User } from '../models/user.model.js';
+import { Chat } from '../models/chat.model.js';
+
+
--- backend/src/models/chat.model.js
+++ backend/src/models/chat.model.js
@@ -1,3 +1,35 @@
-import mongoose  from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const 
+const messageSchema = new Schema(
+    {
+        role: {
+            type: String,
+            enum: ["user", "assistant"],
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    }, {
+        timestamps: true
+    }
+);
+
+const chatSchema = new Schema(
+    {
+        history: {
+            type: Schema.Types.ObjectId,
+            ref: "History",
+            required: true,
+            unique: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    }
+)
+
+export const Chat = mongoose.model("Chat", chatSchema);
